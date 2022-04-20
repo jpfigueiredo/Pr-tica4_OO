2eeeedcdf541dacc4683d0d0e55eaaeb4b4478ab
jpfigueiredo <102195109+jpfigueiredo@users.noreply.github.com>
2022-04-19 22:23:37 -0300

    Add files via upload

---
+++ ContaAbsract/src/ContaMain.java
@@ -0,0 +1,14 @@
+
+public class ContaMain extends ContaPoupanca{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Conta cp = new ContaPoupanca();
+		cp.setSaldo(2121);
+		cp.imprimeExtrato();
+		
+		
+	}
+
+}
---
+++ ContaAbsract/src/ContaPoupanca.java
@@ -0,0 +1,29 @@
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class ContaPoupanca extends Conta {
+
+	public void imprimeExtrato() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("### Extrato da conta ###");
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/aaaa HH:mm:ss");
+		Date date = new Date();
+		
+		System.out.println("Saldo: "+this.getSaldo());
+		System.out.println("Data: "+sdf.format(date));
+		
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
